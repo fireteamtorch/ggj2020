64d7aeba10bf160bb7d318342f6c101e3be6d66b
fireteamtorch <fireteam_torch@yahoo.com>
2020-02-02 13:58:45 -0500

    Added variable manager

---
+++ Assets/Scripts/SceneVariableManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneVariableManager
+{
+
+    public static string originalQuote = "test";
+    public static string editedQuote = "test2";
+
+}
