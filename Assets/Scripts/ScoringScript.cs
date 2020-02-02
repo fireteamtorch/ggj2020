2852f326c86cfaeb288cda048f233ec1bd0e9668
Alice Li <axingyi@gmail.com>
2020-02-01 21:46:41 -0500

    please save the code

--- Assets/Scripts/ScoringScript.cs
+++ Assets/Scripts/ScoringScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ScoringScript : MonoBehaviour
 {
+    /*
+     * 0 = "We must never submit"
+     * 
+     * 
+     */
+
+
+    int score;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,31 @@ public class ScoringScript : MonoBehaviour
     {
         
     }
+
+    int GetScore(string finalText, int id)
+    {
+        score = 0;
+        string txt = finalText.ToLower();
+        switch (id)
+        {
+            case 0:
+                if (txt.Contains("we must submit")) {
+                    score += 40;
+                }
+                if (txt.Contains("they would never take away"))
+                {
+                    score += 40;
+                }
+                if (txt.Contains("only they can lead us"))
+                {
+                    score += 20;
+                }
+                break;
+            default:
+                break;
+        }
+        return score;
+    }
+
+
 }
