a2583eafccde0280b7d24455169f93c7e079bf09
Alice Li <axingyi@gmail.com>
2020-02-02 09:34:16 -0500

    News ticker items

---
+++ Assets/Scripts/TickerItemScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TickerItemScript : MonoBehaviour
+{
+    float tickerWidth;
+    float pixelsPerSec;
+    RectTransform rt;
+
+    public float GetXPosition { get { return rt.anchoredPosition.x; } }
+    public float GetWidth { get { return rt.rect.width; } }
+
+    public void Initialize(float tickerWidth, float pixelsPerSec, string msg)
+    {
+        this.tickerWidth = tickerWidth;
+        this.pixelsPerSec = pixelsPerSec;
+        this.rt = GetComponent<RectTransform>();
+        GetComponent<Text>().text = msg;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rt.position += Vector3.left * pixelsPerSec * Time.deltaTime;
+        if (GetXPosition <= 0 - tickerWidth - GetWidth)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TickerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickerScript : MonoBehaviour
+{
+    public TickerItemScript tickerItemPrefab;
+    public float itemDuration = 10.0f;
+    public string[] fillerItems;
+
+    private float width;
+    private float pixelsPerSec;
+    private TickerItemScript currentItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = GetComponent<RectTransform>().rect.width;
+        pixelsPerSec = width / itemDuration;
+        AddTickerItem("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentItem.GetXPosition <= -currentItem.GetWidth)
+        {
+            AddTickerItem(fillerItems[Random.Range(0, fillerItems.Length)]);
+        }
+    }
+
+    void AddTickerItem(string msg)
+    {
+        currentItem = Instantiate(tickerItemPrefab, transform);
+        currentItem.Initialize(width, pixelsPerSec, msg);
+    }
+}
